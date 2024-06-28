b322e47f81f78f82405bfbc7b1f5836674836eb5
rw <rw@jjk.is>
2024-06-27 21:17:16 -0700

    acme and tailscale

--- flakery.nix
+++ flakery.nix
@@ -1,7 +1,27 @@
 app:
 { config, lib, pkgs, ... }:
 {
+
+    security.acme = {
+    acceptTerms = true;
+    defaults.email = "rwendt1337@gmail.com";
+    certs = {
+      "flakery.xyz" = {
+        domain = domain;
+        # Use DNS challenge for wildcard certificates
+        dnsProvider = "route53"; # Update this to your DNS provider if different
+        environmentFile = "/var/lib/acme/route53-credentials";
+      };
+    };
+  };
+
   networking.firewall.allowedTCPPorts = [ 80 443 ];
+
+  services.tailscale = {
+    enable = true;
+    authKeyFile = "/tsauthkey";
+    extraUpFlags = [ "--ssh" "--hostname" "flakery-load-balancer" ];
+  };
 
   systemd.services.rp = {
     description = "reverse proxy";
