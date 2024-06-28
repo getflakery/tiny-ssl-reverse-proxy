70d50d5991454a440f96a4bcb3e4c96ac0610502
rw <rw@jjk.is>
2024-06-27 21:28:43 -0700

    inline config

--- flake.nix
+++ flake.nix
@@ -22,7 +22,6 @@
           app = callPackage ./. {
             inherit (gomod2nix.legacyPackages.${system}) buildGoApplication;
           };
-          appModule = (import ./flakery.nix) app;
 
         in
         {
@@ -35,7 +34,39 @@
             system = system;
             modules = [
               flakery.nixosModules.flakery
-              appModule
+              {
+                security.acme = {
+                  acceptTerms = true;
+                  defaults.email = "rwendt1337@gmail.com";
+                  certs = {
+                    "flakery.xyz" = {
+                      domain = "*.flakery.xyz";
+                      # Use DNS challenge for wildcard certificates
+                      dnsProvider = "route53"; # Update this to your DNS provider if different
+                      environmentFile = "/var/lib/acme/route53-credentials";
+                    };
+                  };
+                };
+
+                networking.firewall.allowedTCPPorts = [ 80 443 ];
+
+                services.tailscale = {
+                  enable = true;
+                  authKeyFile = "/tsauthkey";
+                  extraUpFlags = [ "--ssh" "--hostname" "flakery-load-balancer" ];
+                };
+
+                systemd.services.rp = {
+                  description = "reverse proxy";
+                  after = [ "network.target" ];
+                  wantedBy = [ "multi-user.target" ];
+                  serviceConfig = {
+                    ExecStart = "${app}/bin/app";
+                    Restart = "always";
+                    KillMode = "process";
+                  };
+                };
+              }
             ];
           };
 
