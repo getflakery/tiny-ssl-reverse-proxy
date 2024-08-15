aaae01df2f2dfbfddea6d9dba77233426c232991
rw <rw@jjk.is>
2024-08-14 21:09:22 -0700

    get this "for free"

--- flake.nix
+++ flake.nix
@@ -118,18 +118,19 @@
                     ];
                   };
                 };
-                services.prometheus = {
-                  enable = true;
-                  port = 9090;
-                  exporters = {
-                    node = {
-                      enable = true;
-                      enabledCollectors = [ "systemd" ];
-                      port = 9002;
-                    };
+                # don't need anymore
+                # services.prometheus = {
+                #   enable = true;
+                #   port = 9090;
+                #   exporters = {
+                #     node = {
+                #       enable = true;
+                #       enabledCollectors = [ "systemd" ];
+                #       port = 9002;
+                #     };
 
-                  };
-                };
+                #   };
+                # };
 
                 # systemd.services.comin = {
                 #   environment = {
