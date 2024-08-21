8f6b385214690d19f4d7aaaedd3095d6b6dee0ac
rw <rw@jjk.is>
2024-08-20 21:12:31 -0700

    assign eip

--- flake.nix
+++ flake.nix
@@ -195,6 +195,36 @@
                   };
                 };
 
+                systemd.services.assign-eip = {
+                  description = "Assign Elastic IP to instance";
+                  wantedBy = [ "multi-user.target" ];
+                  path = with pkgs; [ awscli2 curl ];
+                  environment = {
+                    AWS_ACCESS_KEY_ID = (pkgs.lib.removeSuffix "\n" (builtins.readFile /AWS_ACCESS_KEY_ID));
+                    AWS_SECRET_ACCESS_KEY = (pkgs.lib.removeSuffix "\n" (builtins.readFile /AWS_SECRET_ACCESS_KEY));
+                    AWS_REGION = "us-west-2";
+                  };
+                  script = ''
+                    ELASTIC_IP="44.235.22.226"
+                    INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
+                    ALLOCATION_ID=$(aws ec2 describe-addresses --public-ips $ELASTIC_IP --query 'Addresses[0].AllocationId' --output text)
+                    ASSOCIATION_ID=$(aws ec2 describe-addresses --public-ips $ELASTIC_IP --query 'Addresses[0].AssociationId' --output text)
+
+                    if [ "$ASSOCIATION_ID" != "None" ]; then
+                      echo "Elastic IP is already associated with another instance. Disassociating..."
+                      aws ec2 disassociate-address --association-id "$ASSOCIATION_ID"
+                    fi
+
+                    aws ec2 associate-address --instance-id "$INSTANCE_ID" --allocation-id "$ALLOCATION_ID"
+                  '';
+                  serviceConfig = {
+                    Type = "oneshot";
+                    RemainAfterExit = true;
+                    RestartSec = 32;
+                    Restart = "on-failure";
+                  };
+                };
+
               }
             ];
           };
